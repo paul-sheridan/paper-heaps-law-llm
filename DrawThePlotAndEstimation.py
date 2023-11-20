5185fc42bbaacfa6a1cec516e60f0886f6a059f2
rachelxx03 <53119641+rachelxx03@users.noreply.github.com>
2023-11-20 14:54:23 -0400

    update the table

--- DrawThePlotAndEstimation.py
+++ DrawThePlotAndEstimation.py
@@ -1,57 +1,80 @@
-import math
 import pickle
+
+import pandas as pd
 import numpy as np
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statsmodels.api as sm
 
-class pubMed:
-    def __init__(self):
-        self.data = {}
 
-    def loadDocument(self, filename, label):
-        with open(filename, 'rb') as f:
-            xy = pickle.load(f)
-            x, y = [], []
-            for xAndy in xy:
-                x.append(xAndy[0])
-                y.append(xAndy[1])
-                # x.append(math.log10(xAndy[0]))
-                # y.append(math.log10(xAndy[1]))
-            self.data[label] = (x, y)
+def analyze_corpus(file_path, corpus_name):
+    # Read data
 
-    def DrawHeapLaw(self, step):
-        colors = ['r', 'g', 'b']
-        plt.ylabel('Vocabulary Size')
-        plt.xlabel('Collection Size')
-        plt.title("Heaps' law")
 
-        for idx, (label, (x, y)) in enumerate(self.data.items()):
-            step -= 1
-            i = 0
-            print(x)
-            filtered_x, filtered_y = [], []
-            while i < len(x):
+    with open(file_path, 'rb') as f:
+        data = pickle.load(f)
+    data = data.drop(0)  # Drop the first row
 
-                filtered_x.append(x[i])
-                filtered_y.append(y[i])
-                i += step
-            plt.plot(filtered_x, filtered_y, color=colors[idx], label=label)
-            beta  ,  logk = np.polyfit(filtered_x, filtered_y, 1)
-            print(f"Slope for {label}: {beta}")
-            print(f"Slope for {label}: {10**logk}")
+    # Fit model
+    X = sm.add_constant(np.log10(data['n']))
+    model = sm.OLS(np.log10(data['m']), X).fit()
+
+    # Summary statistics
+    alpha_hat = 10 ** model.params[0]
+    beta_hat = model.params[1]
+    rsq = model.rsquared
+    print(f"{corpus_name} - alpha (est): {alpha_hat:.4f}, beta (est): {beta_hat:.4f}, R squared: {rsq:.4f}")
+
+    # Confidence intervals
+    conf_int = model.conf_int(alpha=0.1)  # 90% CI
+    alpha_ci_low, alpha_ci_high = 10 ** conf_int.iloc[0, 0], 10 ** conf_int.iloc[0, 1]
+    beta_ci_low, beta_ci_high = conf_int.iloc[1, 0], conf_int.iloc[1, 1]
+    print(f"{corpus_name} - 90% CI for alpha: [{alpha_ci_low:.4f}, {alpha_ci_high:.4f}]")
+    print(f"{corpus_name} - 90% CI for beta: [{beta_ci_low:.4f}, {beta_ci_high:.4f}]")
+
+    return data, model.params
 
 
 
 
-        plt.legend()
-        plt.grid(True)
-        plt.savefig('-loglog.pdf', transparent=True)
-        plt.show()
+pubmed_data, pubmed_params = analyze_corpus('data/pubmed.pkl', 'PubMed')
+gptneo125m_data, gptneo125m_params = analyze_corpus('data/heapLawData-gptneo125m.pkl', 'GPT-Neo-125m')
+gptneo13b_data, gptneo13b_params = analyze_corpus('data/heapLawData-gptneo1.3B.pkl', 'GPT-Neo-1.3B')
+gptneo27b_data, gptneo27b_params = analyze_corpus('data/heapLawData-gptneo2.7B.pkl', 'GPT-Neo-2.7B')
 
 
-onehu = pubMed()
-files = [ 'heapLawData-selectedPromt.pkl' , "heaplaw125m.pkl","heaplaw1.3b.pkl"]
-labels = ["PubMed","125m","1.3b" ]
-for file, label in zip(files, labels):
-    onehu.loadDocument(file, label)
 
-onehu.DrawHeapLaw(10)
+def plot_corpus(data, params, corpus_name, ax, log_scale=False):
+    if log_scale:
+        sns.lineplot(x=np.log10(data['n']), y=np.log10(data['m']), ax=ax, label=f"{corpus_name}: β={params[1]:.4f}")
+    else:
+        sns.lineplot(x=data['n'], y=data['m'], ax=ax, label=f"{corpus_name}: β={params[1]:.4f}")
+
+# Natural Scale Plot
+plt.figure(figsize=(12, 8))
+ax1 = plt.subplot(1, 2, 1)
+plot_corpus(pubmed_data, pubmed_params, 'PubMed', ax1)
+plot_corpus(gptneo125m_data, gptneo125m_params, 'GPT-Neo-125m', ax1)
+plot_corpus(gptneo13b_data, gptneo13b_params, 'GPT-Neo-1.3B', ax1)
+plot_corpus(gptneo27b_data, gptneo27b_params, 'GPT-Neo-2.7B', ax1)
+plt.title("Heaps' Law - Natural Scale")
+plt.xlabel('Total Words')
+plt.ylabel('Vocabulary Size')
+plt.legend()
+
+# Log-Log Scale Plot
+ax2 = plt.subplot(1, 2, 2)
+plot_corpus(pubmed_data, pubmed_params, 'PubMed', ax2, log_scale=True)
+plot_corpus(gptneo125m_data, gptneo125m_params, 'GPT-Neo-125m', ax2, log_scale=True)
+plot_corpus(gptneo13b_data, gptneo13b_params, 'GPT-Neo-1.3B', ax2, log_scale=True)
+plot_corpus(gptneo27b_data, gptneo27b_params, 'GPT-Neo-2.7B', ax2, log_scale=True)
+plt.title("Heaps' Law - Log-Log Scale")
+plt.xlabel('Log Total Words')
+plt.ylabel('Log Vocabulary Size')
+plt.legend()
+
+plt.tight_layout()
+# plt.show()
+
+
+
